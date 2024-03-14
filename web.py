c499f2d7c46b4f155b2d76aed66721a26e4247e5
Matthew Segal <matt.segal@biarri.com>
2024-03-14 13:39:56 +1100

    add pdf reading

--- web.py
+++ web.py
@@ -1,9 +1,11 @@
 import sys
 import json
+from io import BytesIO
 
 import requests
 from bs4 import BeautifulSoup
 from trafilatura import extract
+from pypdf import PdfReader
 
 REQUESTS_HEADERS = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
@@ -17,11 +19,18 @@ def main(url: str):
     except (requests.HTTPError, requests.Timeout):
         return
 
-    html = resp.text
-    cleaned_html = BeautifulSoup(html, "html5lib").prettify()
-    contents_raw = extract(cleaned_html, output_format="json")
-    contents = json.loads(contents_raw)
-    print(contents["text"])
+    if resp.headers["content-type"] == "application/pdf":
+        buffer = BytesIO(resp.content)
+        reader = PdfReader(buffer)
+        for page in reader.pages:
+            print(page.extract_text())
+
+    else:
+        html = resp.text
+        cleaned_html = BeautifulSoup(html, "html5lib").prettify()
+        contents_raw = extract(cleaned_html, output_format="json")
+        contents = json.loads(contents_raw)
+        print(contents["text"])
 
 
 if __name__ == "__main__":
